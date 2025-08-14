96ca17a250335d4e5de1b9e9cad85256effa9d25
Hai Nguyen <kevinnguyen27th8@gmail.com>
2025-08-14 14:33:29 +0700

    Add files via upload

---
+++ 1.BT1.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+ 
+using namespace std;
+using ll = long long;
+const int Mod = 1e9+7;
+
+int dist[1000005];
+
+int ror(int x) {
+    int a[7];
+    for (int i = 6; i >= 1; i--) {
+        a[i] = x % 10;
+        x /= 10;
+    }
+    int b[7];
+    b[1] = a[1]; b[2] = a[5]; b[3] = a[2];
+    b[4] = a[4]; b[5] = a[6]; b[6] = a[3];
+    int xr = 0;
+    for (int i = 1; i <= 6; i++) {
+        xr = xr * 10 + b[i];
+    }
+    return xr;
+}
+
+int rol(int x) {
+    int a[7];
+    for (int i = 6; i >= 1; i--) {
+        a[i] = x % 10;
+        x /= 10;
+    }
+    int b[7];
+    b[1] = a[4]; b[2] = a[1]; b[3] = a[3];
+    b[4] = a[5]; b[5] = a[2]; b[6] = a[6];
+    int xl = 0;
+    for (int i = 1; i <= 6; i++) {
+        xl = xl * 10 + b[i];
+    }
+    return xl;
+}
+
+int ton(int a[]) {
+    int res = 0;
+    for (int i = 1; i <= 6; i++) res = res * 10 + a[i];
+    return res;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    //code here
+    memset(dist,0,sizeof(dist));
+    int arr1[7], arr2[7];
+    for (int i = 1; i <= 6; i++) cin >> arr1[i];
+    for (int i = 1; i <= 6; i++) cin >> arr2[i];
+    int st = ton(arr1), ed = ton(arr2);
+    queue<pair<int,int>> q;
+    q.push({st,0});
+    while (!q.empty()) {
+        pair<int,int> p = q.front(); q.pop();
+        if (p.fi == ed) {
+            cout << p.se << '\n';
+            return 0;
+        }
+        int xr = ror(p.fi);
+        int xl = rol(p.fi);
+        if (!dist[xr]) {
+            q.push({xr,p.se+1});
+            dist[xr] = 1;
+        }
+        if (!dist[xl]) {
+            q.push({xl,p.se+1});
+            dist[xl] = 1;
+        }
+    }
+    cout << -1 << '\n';
+}
